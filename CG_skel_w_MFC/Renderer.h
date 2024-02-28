c47492c3738f7d6ff3266720c751a3e51305cf7c
Paulo Khayat <paulokhayat@gmail.com>
2024-02-28 19:26:25 +0200

    Change colors to be vector parameter

--- CG_skel_w_MFC/Renderer.h
+++ CG_skel_w_MFC/Renderer.h
@@ -45,12 +45,12 @@ public:
 	~Renderer(void);
 
 	void ClearBuffer();
-	void FillBuffer(float r, float g, float b);
-	void FillEdges(float amount, float r, float g, float b);
+	void FillBuffer(vec3 color);
+	void FillEdges(float amount, vec3 color);
 	void ResizeBuffers(int new_width, int new_height);
 	void Init();
-	void DrawLine(vec2 vert1, vec2 vert2, float r = 1, float g = 1, float b = 1);
-	void DrawTriangles(const vector<vec3>* vertices, const mat4& world_transform, const vector<vec3>* edge_normals = NULL, bool draw_normals = false, float r = 1, float g = 1, float b = 1, bool color = false);
+	void DrawLine(vec2 vert1, vec2 vert2, vec3 color = vec3(1,1,1));
+	void DrawTriangles(const vector<vec3>* vertices, const mat4& world_transform, const vector<vec3>* edge_normals = NULL, bool draw_normals = false, vec3 edge_color = vec3(1,1,1), bool fill = false);
 	void FillPolygon(const vec2& p1, const vec2& p2, const vec2& p3);
 	void changeColor();
 	vec3 GetColorToFill();
@@ -58,8 +58,8 @@ public:
 	void DrawNormalsToVertices(const vector<vec3>* vertices, const vector<vec3>* vertex_normals = NULL, bool draw_vertex_normals = false);
 
 	void DrawBoundingBox(const vec3* bounding_box, const mat4& world_transform, bool draw_box = false);
-	void DrawPixel(int x, int y, float r, float g, float b);
-	void DrawPixelSafe(int x, int y, float r, float g, float b);
+	void DrawPixel(int x, int y, vec3 color);
+	void DrawPixelSafe(int x, int y, vec3 color);
 	void DrawSymbol(const vec3& vertex ,const mat4& =  mat4(), SYMBOL_TYPE symbol = SYM_STAR, float scale = 1, vec3 colors = vec3(0.6, 0.9, 0.4));
 	void SetCameraTransformInverse(const mat4& cTransform);
 	void SetProjection(const mat4& projection);
