ddbff81d2a62e367436a3848c5070a7f0987a2ac
Paulo Khayat <paulokhayat@gmail.com>
2024-03-07 03:21:09 +0200

    Revert mistakes

--- CG_skel_w_MFC/Renderer.cpp
+++ CG_skel_w_MFC/Renderer.cpp
@@ -343,7 +343,7 @@ vec3 Renderer::phongIllumination(const vec3& surface_point, const vec3& surface_
 	vec3 ambient_color(0.0f, 0.0f, 0.0f);
 	vec3 diffuse_color(0.0f, 0.0f, 0.0f);
 	vec3 specular_color(0.0f, 0.0f, 0.0f);
-	vec3 view_direction = vec3(0,0,1);
+	vec3 view_direction = vec3(0,0,-1);
 
 	for (auto& light : lights) 
 	{
@@ -356,13 +356,13 @@ vec3 Renderer::phongIllumination(const vec3& surface_point, const vec3& surface_
 		// Diffuse component
 		vec3 light_direction = normalize(light_position - surface_point);
 		float cos_theta = max(0.0f, dot(surface_normal, light_direction));
-		diffuse_color += diffuse_color + material.k_diffuse * light->color * color * light->intensity * cos_theta;
+		diffuse_color = diffuse_color + material.k_diffuse * light->color * color * light->intensity * cos_theta;
 
 
 		// Specular component
 		vec3 reflection_direction = reflect(-light_direction, surface_normal);
 		float cos_phi = max(0.0f, dot(reflection_direction, view_direction));
-		specular_color += specular_color + material.k_specular * light->color * color * light->intensity * std::pow(cos_phi, material.k_shiny);
+		specular_color = specular_color + material.k_specular * light->color * color * light->intensity * std::pow(cos_phi, material.k_shiny);
 	}
 
 	vec3 total_color = ambient_color + diffuse_color + specular_color;
