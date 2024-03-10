770ba6d86cfa605fafda66daec735f6cb7a9e697
Paulo Khayat <paulokhayat@gmail.com>
2024-03-10 17:38:37 +0200

    Speed up fog calc with precalculated inverse

--- CG_skel_w_MFC/Fog.h
+++ CG_skel_w_MFC/Fog.h
@@ -16,19 +16,20 @@ private:
 	vec3 color = vec3(0.3, 0.3, 0.3);
 	float m_start = 0;
 	float m_end = 5;
-	float m_end_intensity = 0.8; // what is the alpha channel at max distance
+	float m_inverse_length;
+	float m_end_intensity = 0.8; // what is the alpha channel at max distance (0-1)
 public:
-	Fog() : color(vec3(0.3, 0.3, 0.3)), m_start(0), m_end(5) {}
+	Fog() : color(vec3(0.3, 0.3, 0.3)), m_start(0), m_end(5),m_inverse_length(1/(m_end-m_start)), m_end_intensity(0.9) {}
 	Fog(vec3 color, float start, float end) : color(color), m_start(start), m_end(end) {}
 
 	void setColor(const vec3& new_color) { color = new_color; }
-	void setStart(const float new_start) { m_start = new_start; }
-	void setEnd(const float new_end) { m_end = new_end; }
+	void setStart(const float new_start) { m_start = new_start; m_inverse_length = 1/(m_end-m_start); }
+	void setEnd(const float new_end) { m_end = new_end; m_inverse_length = 1/(m_end-m_start); }
 
 	vec3 getFogColor() const { return color; }
 	float getFogStart() const { return m_start; }
 	float getFogEnd() const { return m_end; }
 
-	float ComputeFog(const float distance) const { float dist = (m_end - distance) / (m_end - m_start); return m_end_intensity*CLAMP(dist, 0, 1); }
+	float ComputeFog(const float distance) const { float dist = (m_end - distance) * m_inverse_length; return m_end_intensity*CLAMP(dist, 0, 1); }
 };
 
