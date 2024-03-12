22474dc6a7fb30a7f3982136462a82c3a9a0a49c
Paulo Khayat <paulokhayat@gmail.com>
2024-03-12 13:52:04 +0200

    Modify color dialog

--- CG_skel_w_MFC/CColorPicker.h
+++ CG_skel_w_MFC/CColorPicker.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "afxdialogex.h"
 #include "afxcolorbutton.h"
-
+#include "util.h"
 // CColorPicker dialog
 
 class CColorPicker : public CDialogEx
@@ -9,7 +9,7 @@ class CColorPicker : public CDialogEx
 	DECLARE_DYNAMIC(CColorPicker)
 
 public:
-	CColorPicker(CWnd* pParent = nullptr);   // standard constructor
+	CColorPicker(CWnd* pParent = nullptr, Light* light = NULL);   // standard constructor
 	virtual ~CColorPicker();
 
 // Dialog Data
@@ -18,6 +18,7 @@ public:
 #endif
 
 protected:
+	Light* light;
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
@@ -27,4 +28,5 @@ public:
 	CSliderCtrl m_slider;
 	CMFCColorButton m_color;
 	int m_sliderval;
+	CStatic m_subtitle;
 };
