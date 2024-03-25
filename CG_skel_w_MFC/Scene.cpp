2ebe048e6f827ce1735ebaf99a2a18d660b93315
Paulo Khayat <paulokhayat@gmail.com>
2024-03-25 14:59:34 +0200

    Add depth buffer

--- CG_skel_w_MFC/Scene.cpp
+++ CG_skel_w_MFC/Scene.cpp
@@ -21,9 +21,11 @@ void Scene::draw()
 
 	glUseProgram(program);
 	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glDepthFunc(GL_LESS);
 
 	for(auto it = models.begin(); it != models.end(); it++){
 		(*(it))->draw(m_renderer);
