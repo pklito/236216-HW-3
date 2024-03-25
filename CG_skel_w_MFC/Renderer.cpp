98abf1e5e86d8670b9e963e3a1904db7f60d6cde
Paulo Khayat <paulokhayat@gmail.com>
2024-03-25 15:51:32 +0200

    Added program vector

--- CG_skel_w_MFC/Renderer.cpp
+++ CG_skel_w_MFC/Renderer.cpp
@@ -10,7 +10,7 @@
 Renderer::Renderer() : Renderer(512,512,"vshader.glsl","fshader.glsl")
 {}
 
-Renderer::Renderer(int width, int height, const char* vshader, const char* fshader) :m_width(width), m_height(height), program(0)
+Renderer::Renderer(int width, int height, const char* vshader, const char* fshader) :m_width(width), m_height(height), programs(), current_program(0)
 {
 	InitOpenGLRendering();
 	CreateBuffers(width,height);
@@ -19,14 +19,14 @@ Renderer::Renderer(int width, int height, const char* vshader, const char* fshad
 
 Renderer::~Renderer(void)
 {
-	if(program != 0){
-		glDeleteProgram(program);
+	for(Program p : programs){
+		glDeleteProgram(p.program);
 	}
 }
 
 void Renderer::StartDraw()
 {
-	glUseProgram(program);
+	glUseProgram(programs[current_program].program);
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -50,11 +50,23 @@ void Renderer::CreateBuffers(int width, int height)
 }
 
 void Renderer::CreateProgram(const char* vshader, const char* fshader){
-	if(program != 0){
-		glDeleteProgram(program);
-	}
-	program = InitShader(vshader, fshader);
+	programs.push_back(Program(InitShader(vshader, fshader),"world_transform","camera_transform",""));
 }
+
+void Renderer::RemoveProgram(int index){
+	if(index >= programs.size())
+		return;
+
+	//Delete the shaders from the GPU
+	glDeleteProgram(programs[index].program);
+
+	//Remove from vector
+	programs.erase(programs.begin() + index);
+
+	//ensure we didn't erase the last program
+	current_program %= programs.size();
+}
+
 
 void Renderer::SetDemoBuffer()
 {
@@ -78,11 +90,11 @@ void Renderer::DrawMesh(GLuint vao, GLuint face_count, const mat4& wm_transform)
 
 	GLfloat full_transform_array[16];
 	toFloatArray(full_transform_array, wm_transform);
-	glUniformMatrix4fv(0, 1, GL_FALSE,full_transform_array);		//TODO get position of "full_transform" uniform
+	glUniformMatrix4fv(programs[current_program].uniform_loc1, 1, GL_FALSE,full_transform_array);		//TODO get position of "full_transform" uniform
 
 	GLfloat proj_array[16];
 	toFloatArray(proj_array, mat_project * mat_transform_inverse);
-	glUniformMatrix4fv(1, 1, GL_FALSE,proj_array);		//TODO get position of "full_transform" uniform
+	glUniformMatrix4fv(programs[current_program].uniform_loc2, 1, GL_FALSE,proj_array);		//TODO get position of "full_transform" uniform
 
 	//Draw
     glDrawArrays(GL_TRIANGLES, 0, face_count*3);
--- CG_skel_w_MFC/Renderer.h
+++ CG_skel_w_MFC/Renderer.h
@@ -7,6 +7,28 @@
 
 class Camera;
 
+class Program {
+	private:
+		GLuint get_uniform(GLuint program, const char* name){
+			return glGetUniformLocation(program, name);
+		}
+		
+		
+	public:
+		GLuint program;
+		GLuint uniform_loc1;
+		GLuint uniform_loc2;
+		GLuint uniform_loc3;
+	Program() : program(0), uniform_loc1(-1), uniform_loc2(-1), uniform_loc3(-1) {};
+	Program(GLuint programID, GLuint uniformLocation1, GLuint uniformLocation2, GLuint uniformLocation3)
+        : program(programID), uniform_loc1(uniformLocation1), uniform_loc2(uniformLocation2), uniform_loc3(uniformLocation3) {};
+	Program(GLuint programID,const char* name1,const char* name2,const char* name3) : program(programID){
+		uniform_loc1=get_uniform(program,name1);
+		uniform_loc2=get_uniform(program,name2);
+		uniform_loc3=get_uniform(program,name3);
+	};
+};
+
 class Renderer
 {
 	float *m_outBuffer; // 3*width*height
@@ -14,7 +36,8 @@ class Renderer
 	int m_width, m_height;
 
 	//vertex/fragment shader
-	GLuint program;
+	std::vector<Program> programs;
+	int current_program;
 
 	//current camera storage
 	mat4 mat_transform_inverse;
@@ -22,7 +45,6 @@ class Renderer
 	vec3 camera_position;
 
 	void CreateBuffers(int width, int height);
-	void CreateProgram(const char* vshader, const char* fshader);
 	void CreateLocalBuffer();
 
 	//////////////////////////////
@@ -37,12 +59,14 @@ public:
 	Renderer();
 	Renderer(int width, int height, const char* vshader, const char* fshader);
 	~Renderer(void);
+	void Init();
 
 	void StartDraw();
 	void EndDraw();
 
-	void Init();
-	
+	void CreateProgram(const char* vshader, const char* fshader);
+	void RemoveProgram(int index);
+
 	void DrawMesh(GLuint vao,GLuint face_count, const mat4& transform);
 	void SetCameraTransformInverse(const mat4& cTransform);
 	void SetProjection(const mat4& projection);
