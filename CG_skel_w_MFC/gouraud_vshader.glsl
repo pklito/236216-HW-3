46efc0b47d9d2ad46bb9e9219ca35d6115bed44c
Paulo Khayat <paulokhayat@gmail.com>
2024-04-01 16:25:13 +0300

    clone phong shader to gouraud and flat

---
+++ CG_skel_w_MFC/gouraud_vshader.glsl
@@ -0,0 +1,32 @@
+#version 330
+
+layout (location = 0) in  vec3 vPosition;
+layout (location = 1) in  vec3 vNormal;
+layout (location = 2) in  vec3 vTexture;
+layout (location = 3) in  vec3 vMatAmb;
+layout (location = 4) in  vec3 vMatDiff;
+layout (location = 5) in  vec3 vMatSpec;
+uniform mat4 world_transform;
+uniform mat4 camera_transform;
+uniform mat4 normal_transform;
+varying vec3 fNormal;
+varying vec2 fTexture;
+varying vec4 fPos;
+
+varying vec3 fDiffuse;
+varying vec3 fSpecular;
+varying vec3 fAmbient;
+void main()
+{
+    //world position for lights
+    vec4 world_pos = world_transform * vec4(vPosition,1.0);
+    fPos = world_pos;
+    fNormal = normalize((normal_transform * vec4(vNormal,1.0)).xyz);
+    fTexture = vTexture.xy;
+
+    fAmbient = vMatAmb;
+    fDiffuse = vMatDiff;
+    fSpecular = vMatSpec;
+    //screen position
+    gl_Position = camera_transform * world_pos;
+}
